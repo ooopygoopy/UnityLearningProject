2569fc87b00db06400aa0324b17b6f12194f0880
Caleb Kelley <cak8833@gmail.com>
2026-02-07 05:55:05 -0500

    Continued Movement work

--- LearningProject/Assets/Scripts/PlayerInputHandler.cs
+++ LearningProject/Assets/Scripts/PlayerInputHandler.cs
@@ -29,10 +29,76 @@ public class PlayerInputHandler : MonoBehaviour
     private InputAction sprintAction;
 
     //Gets the values of each of the inputs to see if they have been pressed
+    //Use getters/setters to make controls more protected
     public Vector2 MoveInput { get; private set;  }
-    public Vector2 Look { get; private set;  }
+    public Vector2 LookInput { get; private set;  }
     public bool JumpTriggered { get; private set;  }
     public float SprintValue { get; private set;  }
+
+    //Use script as instance/singleton to allow reference from other scripts
+    public static PlayerInputHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+
+        // makes sure there are no duplicates of this script
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        //allows reference to actions in order to use them
+        moveAction = playerControls.FindActionMap(actionMapName).FindAction(move);
+        lookAction = playerControls.FindActionMap(actionMapName).FindAction(look);
+        jumpAction = playerControls.FindActionMap(actionMapName).FindAction(jump);
+        sprintAction = playerControls.FindActionMap(actionMapName).FindAction(sprint);
+        //calls the register input actions function
+        RegisterInputActions();
+    }
+
+    //registers actions
+    void RegisterInputActions()
+    {
+        //if the move actions happes check the value of move input vector
+        moveAction.performed += context => MoveInput = context.ReadValue<Vector2>();
+        //if the move action canceld set the value of move input vector zero
+        moveAction.canceled += context => MoveInput = Vector2.zero;
+
+        lookAction.performed += context => LookInput = context.ReadValue<Vector2>();
+        lookAction.canceled += context => LookInput = Vector2.zero;
+
+        jumpAction.performed += context => JumpTriggered = true;
+        jumpAction.canceled += context => JumpTriggered = false;
+
+        sprintAction.performed += context => SprintValue = context.ReadValue<float>();
+        sprintAction.canceled += context => SprintValue = 0f;
+
+
+    }
+
+    //Allows to enable events when they are needed
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        lookAction.Enable();
+        jumpAction.Enable();
+        sprintAction.Enable();
+    }
+
+    //Allows to disable events when they are needed
+    //ex used when the game or script ends or scene change
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        lookAction.Disable();
+        jumpAction.Disable();
+        sprintAction.Disable();
+    }
 
 
 }
